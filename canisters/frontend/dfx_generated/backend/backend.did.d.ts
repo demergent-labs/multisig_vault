52c8c8a50a8edae8999228782fbb7e9b7fd1278d
Jordan Last <jordan.michael.last@gmail.com>
2022-07-21 16:43:28 -0500

    new test

--- canisters/frontend/dfx_generated/backend/backend.did.d.ts
+++ canisters/frontend/dfx_generated/backend/backend.did.d.ts
@@ -1,291 +1,238 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export interface AccountBalanceArgs {
-    account: Array<number>;
-}
-export interface Archive {
-    canister_id: Principal;
-}
-export interface Archives {
-    archives: Array<Archive>;
-}
+export interface AccountBalanceArgs { 'account' : Array<number> }
+export interface Archive { 'canister_id' : Principal }
+export interface Archives { 'archives' : Array<Archive> }
 export interface Block {
-    transaction: Transaction;
-    timestamp: TimeStamp;
-    parent_hash: [] | [Array<number>];
+  'transaction' : Transaction,
+  'timestamp' : TimeStamp,
+  'parent_hash' : [] | [Array<number>],
 }
-export interface BlockRange {
-    blocks: Array<Block>;
-}
+export interface BlockRange { 'blocks' : Array<Block> }
 export interface CanisterSettings {
-    freezing_threshold: [] | [bigint];
-    controllers: [] | [Array<Principal>];
-    memory_allocation: [] | [bigint];
-    compute_allocation: [] | [bigint];
+  'freezing_threshold' : [] | [bigint],
+  'controllers' : [] | [Array<Principal>],
+  'memory_allocation' : [] | [bigint],
+  'compute_allocation' : [] | [bigint],
 }
-export type CanisterStatus =
-    | { stopped: null }
-    | { stopping: null }
-    | { running: null };
-export interface CanisterStatusArgs {
-    canister_id: Principal;
-}
+export type CanisterStatus = { 'stopped' : null } |
+  { 'stopping' : null } |
+  { 'running' : null };
+export interface CanisterStatusArgs { 'canister_id' : Principal }
 export interface CanisterStatusResult {
-    status: CanisterStatus;
-    memory_size: bigint;
-    cycles: bigint;
-    settings: DefiniteCanisterSettings;
-    module_hash: [] | [Array<number>];
+  'status' : CanisterStatus,
+  'memory_size' : bigint,
+  'cycles' : bigint,
+  'settings' : DefiniteCanisterSettings,
+  'module_hash' : [] | [Array<number>],
 }
 export interface ControllersInfo {
-    frontend: Array<Principal>;
-    backend: Array<Principal>;
+  'frontend' : Array<Principal>,
+  'backend' : Array<Principal>,
 }
-export type ControllersInfoResult = { ok: ControllersInfo } | { err: string };
-export interface CreateCanisterArgs {
-    settings: [] | [CanisterSettings];
-}
-export interface CreateCanisterResult {
-    canister_id: Principal;
-}
+export type ControllersInfoResult = { 'ok' : ControllersInfo } |
+  { 'err' : string };
+export interface CreateCanisterArgs { 'settings' : [] | [CanisterSettings] }
+export interface CreateCanisterResult { 'canister_id' : Principal }
 export interface CycleStatsInfo {
-    frontend: {
-        cycles_remaining: bigint;
-        cycles_per_day: bigint;
-        cycles_per_min: bigint;
-        cycles_per_sec: bigint;
-        cycles_per_month: bigint;
-        cycles_per_hour: bigint;
-        cycles_per_week: bigint;
-        cycles_per_year: bigint;
-        cycle_time_remaining: bigint;
-    };
-    backend: {
-        cycles_remaining: bigint;
-        cycles_per_day: bigint;
-        cycles_per_min: bigint;
-        cycles_per_sec: bigint;
-        cycles_per_month: bigint;
-        cycles_per_hour: bigint;
-        cycles_per_week: bigint;
-        cycles_per_year: bigint;
-        cycle_time_remaining: bigint;
-    };
+  'frontend' : {
+    'cycles_remaining' : bigint,
+    'cycles_per_day' : bigint,
+    'cycles_per_min' : bigint,
+    'cycles_per_sec' : bigint,
+    'cycles_per_month' : bigint,
+    'cycles_per_hour' : bigint,
+    'cycles_per_week' : bigint,
+    'cycles_per_year' : bigint,
+    'cycle_time_remaining' : bigint,
+  },
+  'backend' : {
+    'cycles_remaining' : bigint,
+    'cycles_per_day' : bigint,
+    'cycles_per_min' : bigint,
+    'cycles_per_sec' : bigint,
+    'cycles_per_month' : bigint,
+    'cycles_per_hour' : bigint,
+    'cycles_per_week' : bigint,
+    'cycles_per_year' : bigint,
+    'cycle_time_remaining' : bigint,
+  },
 }
-export interface DecimalsResult {
-    decimals: number;
-}
-export type DefaultResult = { ok: boolean } | { err: string };
+export interface DecimalsResult { 'decimals' : number }
+export type DefaultResult = { 'ok' : boolean } |
+  { 'err' : string };
 export interface DefiniteCanisterSettings {
-    freezing_threshold: bigint;
-    controllers: Array<Principal>;
-    memory_allocation: bigint;
-    compute_allocation: bigint;
+  'freezing_threshold' : bigint,
+  'controllers' : Array<Principal>,
+  'memory_allocation' : bigint,
+  'compute_allocation' : bigint,
 }
-export interface DeleteCanisterArgs {
-    canister_id: Principal;
-}
-export interface DepositCyclesArgs {
-    canister_id: Principal;
-}
-export interface GetBlocksArgs {
-    start: bigint;
-    length: bigint;
-}
+export interface DeleteCanisterArgs { 'canister_id' : Principal }
+export interface DepositCyclesArgs { 'canister_id' : Principal }
+export interface GetBlocksArgs { 'start' : bigint, 'length' : bigint }
 export interface InstallCodeArgs {
-    arg: Array<number>;
-    wasm_module: Array<number>;
-    mode: InstallCodeMode;
-    canister_id: Principal;
+  'arg' : Array<number>,
+  'wasm_module' : Array<number>,
+  'mode' : InstallCodeMode,
+  'canister_id' : Principal,
 }
-export type InstallCodeMode =
-    | { reinstall: null }
-    | { upgrade: null }
-    | { install: null };
-export interface NameResult {
-    name: string;
-}
-export type Operation =
-    | {
-          Burn: { from: Array<number>; amount: Tokens };
-      }
-    | { Mint: { to: Array<number>; amount: Tokens } }
-    | {
-          Transfer: {
-              to: Array<number>;
-              fee: Tokens;
-              from: Array<number>;
-              amount: Tokens;
-          };
-      };
+export type InstallCodeMode = { 'reinstall' : null } |
+  { 'upgrade' : null } |
+  { 'install' : null };
+export interface NameResult { 'name' : string }
+export type Operation = {
+    'Burn' : { 'from' : Array<number>, 'amount' : Tokens }
+  } |
+  { 'Mint' : { 'to' : Array<number>, 'amount' : Tokens } } |
+  {
+    'Transfer' : {
+      'to' : Array<number>,
+      'fee' : Tokens,
+      'from' : Array<number>,
+      'amount' : Tokens,
+    }
+  };
 export interface ProvisionalCreateCanisterWithCyclesArgs {
-    settings: [] | [CanisterSettings];
-    amount: [] | [bigint];
+  'settings' : [] | [CanisterSettings],
+  'amount' : [] | [bigint],
 }
 export interface ProvisionalCreateCanisterWithCyclesResult {
-    canister_id: Principal;
+  'canister_id' : Principal,
 }
 export interface ProvisionalTopUpCanisterArgs {
-    canister_id: Principal;
-    amount: bigint;
+  'canister_id' : Principal,
+  'amount' : bigint,
 }
-export type QueryArchiveError =
-    | {
-          BadFirstBlockIndex: {
-              requested_index: bigint;
-              first_valid_index: bigint;
-          };
-      }
-    | { Other: { error_message: string; error_code: bigint } };
+export type QueryArchiveError = {
+    'BadFirstBlockIndex' : {
+      'requested_index' : bigint,
+      'first_valid_index' : bigint,
+    }
+  } |
+  { 'Other' : { 'error_message' : string, 'error_code' : bigint } };
 export type QueryArchiveFn = ActorMethod<[GetBlocksArgs], QueryArchiveResult>;
-export type QueryArchiveResult =
-    | { Ok: BlockRange }
-    | { Err: QueryArchiveError };
+export type QueryArchiveResult = { 'Ok' : BlockRange } |
+  { 'Err' : QueryArchiveError };
 export interface QueryBlocksResponse {
-    certificate: [] | [Array<number>];
-    blocks: Array<Block>;
-    chain_length: bigint;
-    first_block_index: bigint;
-    archived_blocks: Array<{
-        callback: QueryArchiveFn;
-        start: bigint;
-        length: bigint;
-    }>;
+  'certificate' : [] | [Array<number>],
+  'blocks' : Array<Block>,
+  'chain_length' : bigint,
+  'first_block_index' : bigint,
+  'archived_blocks' : Array<
+    { 'callback' : QueryArchiveFn, 'start' : bigint, 'length' : bigint }
+  >,
 }
 export interface SignerProposal {
-    id: string;
-    remove: boolean;
-    rejected_at: [] | [bigint];
-    adopted_at: [] | [bigint];
-    votes: Array<Vote>;
-    description: string;
-    created_at: bigint;
-    rejected: boolean;
-    proposer: Principal;
-    signer: Principal;
-    adopted: boolean;
+  'id' : string,
+  'remove' : boolean,
+  'rejected_at' : [] | [bigint],
+  'adopted_at' : [] | [bigint],
+  'votes' : Array<Vote>,
+  'description' : string,
+  'created_at' : bigint,
+  'rejected' : boolean,
+  'proposer' : Principal,
+  'signer' : Principal,
+  'adopted' : boolean,
 }
-export interface StartCanisterArgs {
-    canister_id: Principal;
-}
-export interface StopCanisterArgs {
-    canister_id: Principal;
-}
-export interface SymbolResult {
-    symbol: string;
-}
+export interface StartCanisterArgs { 'canister_id' : Principal }
+export interface StopCanisterArgs { 'canister_id' : Principal }
+export interface SymbolResult { 'symbol' : string }
 export interface ThresholdProposal {
-    id: string;
-    rejected_at: [] | [bigint];
-    adopted_at: [] | [bigint];
-    threshold: number;
-    votes: Array<Vote>;
-    description: string;
-    created_at: bigint;
-    rejected: boolean;
-    proposer: Principal;
-    adopted: boolean;
+  'id' : string,
+  'rejected_at' : [] | [bigint],
+  'adopted_at' : [] | [bigint],
+  'threshold' : number,
+  'votes' : Array<Vote>,
+  'description' : string,
+  'created_at' : bigint,
+  'rejected' : boolean,
+  'proposer' : Principal,
+  'adopted' : boolean,
 }
-export interface TimeStamp {
-    timestamp_nanos: bigint;
-}
-export interface Tokens {
-    e8s: bigint;
-}
+export interface TimeStamp { 'timestamp_nanos' : bigint }
+export interface Tokens { 'e8s' : bigint }
 export interface Transaction {
-    memo: bigint;
-    operation: [] | [Operation];
-    created_at_time: TimeStamp;
+  'memo' : bigint,
+  'operation' : [] | [Operation],
+  'created_at_time' : TimeStamp,
 }
-export interface Transfer {
-    id: string;
-    to: Principal;
-    amount: bigint;
-}
+export interface Transfer { 'id' : string, 'to' : Principal, 'amount' : bigint }
 export interface TransferArgs {
-    to: Array<number>;
-    fee: Tokens;
-    memo: bigint;
-    from_subaccount: [] | [Array<number>];
-    created_at_time: [] | [TimeStamp];
-    amount: Tokens;
+  'to' : Array<number>,
+  'fee' : Tokens,
+  'memo' : bigint,
+  'from_subaccount' : [] | [Array<number>],
+  'created_at_time' : [] | [TimeStamp],
+  'amount' : Tokens,
 }
-export type TransferError =
-    | {
-          TxTooOld: { allowed_window_nanos: bigint };
-      }
-    | { BadFee: { expected_fee: Tokens } }
-    | { TxDuplicate: { duplicate_of: bigint } }
-    | { TxCreatedInFuture: null }
-    | { InsufficientFunds: { balance: Tokens } };
-export interface TransferFee {
-    transfer_fee: Tokens;
-}
+export type TransferError = {
+    'TxTooOld' : { 'allowed_window_nanos' : bigint }
+  } |
+  { 'BadFee' : { 'expected_fee' : Tokens } } |
+  { 'TxDuplicate' : { 'duplicate_of' : bigint } } |
+  { 'TxCreatedInFuture' : null } |
+  { 'InsufficientFunds' : { 'balance' : Tokens } };
+export interface TransferFee { 'transfer_fee' : Tokens }
 export type TransferFeeArg = {};
 export interface TransferProposal {
-    id: string;
-    destination_address: string;
-    rejected_at: [] | [bigint];
-    adopted_at: [] | [bigint];
-    votes: Array<Vote>;
-    description: string;
-    created_at: bigint;
-    rejected: boolean;
-    proposer: Principal;
-    amount: bigint;
-    adopted: boolean;
+  'id' : string,
+  'destination_address' : string,
+  'rejected_at' : [] | [bigint],
+  'adopted_at' : [] | [bigint],
+  'votes' : Array<Vote>,
+  'description' : string,
+  'created_at' : bigint,
+  'rejected' : boolean,
+  'proposer' : Principal,
+  'amount' : bigint,
+  'adopted' : boolean,
 }
-export type TransferResult = { Ok: bigint } | { Err: TransferError };
-export interface UninstallCodeArgs {
-    canister_id: Principal;
-}
+export type TransferResult = { 'Ok' : bigint } |
+  { 'Err' : TransferError };
+export interface UninstallCodeArgs { 'canister_id' : Principal }
 export interface UpdateSettingsArgs {
-    canister_id: Principal;
-    settings: CanisterSettings;
+  'canister_id' : Principal,
+  'settings' : CanisterSettings,
 }
-export type VaultBalanceResult = { ok: bigint } | { err: string };
-export interface Vote {
-    adopt: boolean;
-    voter: Principal;
-}
-export type VoteOnProposalAction =
-    | { voted: null }
-    | { rejected: null }
-    | { adopted: null };
-export type VoteOnProposalResult =
-    | { ok: VoteOnProposalAction }
-    | { err: string };
-export type VoteOnTransferProposalResult =
-    | { ok: VoteOnProposalAction }
-    | { err: { transfer_error: TransferError } | { message: string } };
+export type VaultBalanceResult = { 'ok' : bigint } |
+  { 'err' : string };
+export interface Vote { 'adopt' : boolean, 'voter' : Principal }
+export type VoteOnProposalAction = { 'voted' : null } |
+  { 'rejected' : null } |
+  { 'adopted' : null };
+export type VoteOnProposalResult = { 'ok' : VoteOnProposalAction } |
+  { 'err' : string };
+export type VoteOnTransferProposalResult = { 'ok' : VoteOnProposalAction } |
+  { 'err' : { 'transfer_error' : TransferError } | { 'message' : string } };
 export interface _SERVICE {
-    get_address_from_principal: ActorMethod<[Principal], string>;
-    get_canister_address: ActorMethod<[], string>;
-    get_canister_principal: ActorMethod<[], Principal>;
-    get_controllers_info: ActorMethod<[], ControllersInfoResult>;
-    get_cycle_stats_info: ActorMethod<[], CycleStatsInfo>;
-    get_signer_proposals: ActorMethod<[], Array<SignerProposal>>;
-    get_signers: ActorMethod<[], Array<Principal>>;
-    get_threshold: ActorMethod<[], number>;
-    get_threshold_proposals: ActorMethod<[], Array<ThresholdProposal>>;
-    get_transfer_proposals: ActorMethod<[], Array<TransferProposal>>;
-    get_transfers: ActorMethod<[], Array<Transfer>>;
-    get_vault_balance: ActorMethod<[], VaultBalanceResult>;
-    propose_signer: ActorMethod<[string, Principal, boolean], DefaultResult>;
-    propose_threshold: ActorMethod<[string, number], DefaultResult>;
-    propose_transfer: ActorMethod<[string, string, bigint], DefaultResult>;
-    snapshot_cycles: ActorMethod<[], DefaultResult>;
-    vote_on_signer_proposal: ActorMethod<
-        [string, boolean],
-        VoteOnProposalResult
-    >;
-    vote_on_threshold_proposal: ActorMethod<
-        [string, boolean],
-        VoteOnProposalResult
-    >;
-    vote_on_transfer_proposal: ActorMethod<
-        [string, boolean],
-        VoteOnTransferProposalResult
-    >;
+  'get_address_from_principal' : ActorMethod<[Principal], string>,
+  'get_canister_address' : ActorMethod<[], string>,
+  'get_canister_principal' : ActorMethod<[], Principal>,
+  'get_controllers_info' : ActorMethod<[], ControllersInfoResult>,
+  'get_cycle_stats_info' : ActorMethod<[], CycleStatsInfo>,
+  'get_signer_proposals' : ActorMethod<[], Array<SignerProposal>>,
+  'get_signers' : ActorMethod<[], Array<Principal>>,
+  'get_threshold' : ActorMethod<[], number>,
+  'get_threshold_proposals' : ActorMethod<[], Array<ThresholdProposal>>,
+  'get_transfer_proposals' : ActorMethod<[], Array<TransferProposal>>,
+  'get_transfers' : ActorMethod<[], Array<Transfer>>,
+  'get_vault_balance' : ActorMethod<[], VaultBalanceResult>,
+  'propose_signer' : ActorMethod<[string, Principal, boolean], DefaultResult>,
+  'propose_threshold' : ActorMethod<[string, number], DefaultResult>,
+  'propose_transfer' : ActorMethod<[string, string, bigint], DefaultResult>,
+  'snapshot_cycles' : ActorMethod<[], DefaultResult>,
+  'vote_on_signer_proposal' : ActorMethod<
+    [string, boolean],
+    VoteOnProposalResult,
+  >,
+  'vote_on_threshold_proposal' : ActorMethod<
+    [string, boolean],
+    VoteOnProposalResult,
+  >,
+  'vote_on_transfer_proposal' : ActorMethod<
+    [string, boolean],
+    VoteOnTransferProposalResult,
+  >,
 }
--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,18 @@
-import { deploy, run_tests, Test } from 'azle/test';
+import { run_tests, Test } from 'azle/test';
 import { execSync } from 'child_process';
+import { createActor } from '../canisters/frontend/dfx_generated/backend';
+
+// TODO need to get ICP Ledger and Internet Identity canisters working in GitHub Actions
+
+const backend_canister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
+    agentOptions: {
+        host: 'http://127.0.0.1:8000'
+    }
+});
 
 const tests: Test[] = [
     {
-        name: 'build',
+        name: 'deploy backend canister',
         prep: async () => {
             execSync(`dfx canister create backend`, {
                 stdio: 'inherit'
@@ -12,8 +21,61 @@ const tests: Test[] = [
             execSync(`dfx build backend`, {
                 stdio: 'inherit'
             });
+
+            execSync(
+                `dfx canister install backend --argument '(vec { principal "'$(dfx identity get-principal)'" }, 1: nat8)' --wasm target/wasm32-unknown-unknown/release/backend.wasm.gz`,
+                {
+                    stdio: 'inherit'
+                }
+            );
         }
     }
+    // {
+    //     name: 'deploy frontend canister',
+    //     prep: async () => {
+    //         execSync(`dfx deploy frontend`, {
+    //             stdio: 'inherit'
+    //         });
+    //     }
+    // },
+    // {
+    //     name: 'deploy Internet Identity canister',
+    //     prep: async () => {
+    //         execSync(`dfx deploy internet_identity --argument '(null)'`, {
+    //             stdio: 'inherit'
+    //         });
+    //     }
+    // },
+    // {
+    //     name: 'deploy ICP Ledger canister',
+    //     prep: async () => {
+    //         execSync(`dfx deploy icp_ledger --argument='(record {minting_account = "'$(dfx ledger account-id)'"; initial_values = vec { record { "'$(dfx ledger account-id --of-canister backend)'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'`, {
+    //             stdio: 'inherit'
+    //         });
+    //     }
+    // },
+    // {
+    //     name: 'set controllers',
+    //     prep: async () => {
+    //         execSync(`dfx canister update-settings --add-controller $(dfx canister id backend) frontend`, {
+    //             stdio: 'inherit'
+    //         });
+
+    //         execSync(`dfx canister update-settings --add-controller $(dfx canister id backend) backend`, {
+    //             stdio: 'inherit'
+    //         });
+    //     }
+    // },
+    // {
+    //     name: 'check vault balance',
+    //     test: async () => {
+    //         const result = await backend_canister.get_vault_balance();
+
+    //         return {
+    //             ok: 'ok' in result && result.ok === 100_000_000n
+    //         };
+    //     }
+    // }
 ];
 
 run_tests(tests);
