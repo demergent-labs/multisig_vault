4a41a5439a7ee38f3f9c941357d8b07ab48b6771
Jordan Last <jordan.michael.last@gmail.com>
2022-07-21 14:51:30 -0500

    add build to test

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,15 @@
 import { deploy, run_tests, Test } from 'azle/test';
+import { execSync } from 'child_process';
 
-const tests: Test[] = [];
+const tests: Test[] = [
+    {
+        name: 'build',
+        prep: async () => {
+            execSync(`dfx build backend`, {
+                stdio: 'inherit'
+            });
+        }
+    }
+];
 
 run_tests(tests);
