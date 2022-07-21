489c635128ea17e01cc8d5ea19ce48507dedac61
Jordan Last <jordan.michael.last@gmail.com>
2022-07-21 14:53:46 -0500

    test

--- test/test.ts
+++ test/test.ts
@@ -5,6 +5,10 @@ const tests: Test[] = [
     {
         name: 'build',
         prep: async () => {
+            execSync(`dfx canister create backend`, {
+                stdio: 'inherit'
+            });
+
             execSync(`dfx build backend`, {
                 stdio: 'inherit'
             });
